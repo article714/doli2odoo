d0e824f7415c5fe3c5ec7ef95394a5c5c734c600
xtof <christophe@openflexo.org>
2018-03-05 11:54:45 +0100

    [WiP] Account.account and Account.journal for bank accounts

--- scripts/dataprocessors/bank_account.py
+++ scripts/dataprocessors/bank_account.py
@@ -22,11 +22,21 @@ def process (logger, odooenv, odoocr, dolidb):
 
     res_bank_model = odooenv['res.bank']
     account_journal_model = odooenv['account.journal']
+    account_account_model = odooenv['account.account']
+    account_account_type_model = odooenv['account.account.type']
+
+    nestedquery = "select distinct iban_prefix,label,account_number FROM llx_bank_account where bank=%s;"
+
+    # looking for bank account type
+
+    found = account_account_type_model.search(['|', ('name', 'ilike', '%bank%'), ('name', 'ilike', '%banq%')], limit = 1)
+    if len(found) == 1:
+        bank_account_type_id = found[0].id
+
+    # loop over bank accounts to find banks
 
     dolicursor = dolidb.cursor()
     dolicursor.execute("select distinct bank, bic FROM llx_bank_account;")
-
-    nestedquery = "select distinct iban_prefix FROM llx_bank_account where bank=%s;"
 
     for (bank, bic) in dolicursor.fetchall():
 
@@ -47,18 +57,38 @@ def process (logger, odooenv, odoocr, dolidb):
             dolipcursor = dolidb.cursor()
             dolipcursor.execute(nestedquery, (bank,))
 
-            for (iban,) in dolipcursor.fetchall():
+            for (iban, label, account_number) in dolipcursor.fetchall():
                 iban = toString(iban).replace(' ', '').strip()
-                found = account_journal_model.search([('bank_acc_number', '=', iban)])
 
-                values = {'type':'bank', 'bank_acc_number':iban,
-                         'bank_id': a_bank.id}
+                # Account
+                found = account_account_model.search([('code', '=', account_number)])
+
+                values = {'user_type_id':bank_account_type_id, 'code':account_number,
+                          'name':label}
 
                 if len(found) == 1:
-                    an_account = found[0]
-                    an_account.write(values)
+                    the_account = found[0]
+                    the_account.write(values)
                 else:
-                    an_account = account_journal_model.create(values)
+                    the_account = account_account_model.create(values)
+
+                # Journal
+
+                found = account_journal_model.search(['|', ('bank_acc_number', '=', iban), ('name', '=', label)])
+                values = {'type':'bank',
+                          'name':label,
+                         'bank_id': a_bank.id}
+                if iban != None and len(iban) > 2:
+                    values['bank_acc_number'] = iban
+                if the_account != None:
+                    values['default_debit_account_id'] = the_account.id
+                    values['default_credit_account_id'] = the_account.id
+
+                if len(found) == 1:
+                    the_journal = found[0]
+                    the_journal.write(values)
+                else:
+                    the_journal = account_journal_model.create(values)
 
             odoocr.commit()
             dolipcursor.close()
